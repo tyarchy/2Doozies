d9d3680a84d631fada21209c7eb202699e1ac869
jstable1 <jessica.stabler@gmail.com>
2022-03-24 20:10:08 -0600

    updated gitinore file and server.js file to use express

---
+++ controllers/home-routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./config/connection.js');
+
+// // Display all to-dos
+// router.get('/api/doozies', (req, res) => {
+
+// })
+
+// Create new to-do
+
+// Update to-do
+
+// Delete to-do
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-const schedule = require('node-schedule');
+const express = require('express');
+const sequelize = require('./config/connection');
+// const schedule = require('node-schedule');
+const apiRoutes = require('./controllers/home-routes.js');
 
 const PORT = process.env.PORT || 3001;
-const app = schedule ();
+const app = express ();
 
-odafoasdf
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server after DB connection
+sequelize.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+});
