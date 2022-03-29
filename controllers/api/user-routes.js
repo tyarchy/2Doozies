928bbb10c17651f3f8f14c33da4547ddbc9a7670
nichojohnson84 <nickcjohnson84@outlook.com>
2022-03-29 11:41:20 -0600

    login and logout are working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
-const router = require("express").Router();
-const { User } = require("../../models");
+const router = require('express').Router();
+const { User } = require('../../models');
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
@@ -23,7 +23,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
@@ -31,14 +31,14 @@ router.post("/login", (req, res) => {
     },
   }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: "No user with that email address!" });
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: "Incorrect password!" });
+      res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
@@ -47,19 +47,19 @@ router.post("/login", (req, res) => {
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
 
-      res.json({ user: dbUserData, message: "You are now logged in!" });
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
 });
 
-router.post("/logout", (req, res) => {
+router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
-    res.status(404).end();
+    res.status(500).end();
   }
 });
 
-module.exports = routes;
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,34 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { User, Doozie } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Doozie } = require('../models');
 
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect('/');
     return;
   }
-  res.render("login");
+  res.render('login');
 });
 
 router.get('/', (req, res) => {
-  console.log('======================');
-  Doozie.findAll({
-    attributes: [
-      'title',
-      'description',
-      'created_at',
-    ],
-  })
-    .then(dbDoozieData => {
-      const doozie = dbDoozieData.map(doozie => doozie.get({ plain: true }));
-
-      res.render('dashboard', {doozie});
+  if (req.session.loggedIn) {
+    Doozie.findAll({
+      attributes: ['title', 'description', 'created_at'],
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then((dbDoozieData) => {
+        const doozie = dbDoozieData.map((doozie) =>
+          doozie.get({ plain: true })
+        );
+
+        res.render('dashboard', { doozie, loggedIn: true });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  } else {
+    res.render('login');
+  }
 });
 
 module.exports = router;
