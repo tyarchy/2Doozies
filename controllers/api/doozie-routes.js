b80d953946db36a242a0ff27dffd686084313596
jstable1 <jessica.stabler@gmail.com>
2022-03-30 13:35:33 -0600

    updated routes to start to address to-do's not posting issue

--- controllers/api/doozie-routes.js
+++ controllers/api/doozie-routes.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
-const { Doozie } = require('../../models');
+const { Doozie, User } = require('../../models');
 
 // GET - display all doozies
 router.get('/', (req, res) => {
-  Doozie.findAll()
+  Doozie.findAll({
+    attributes: [
+      'id',
+      'title',
+      'description',
+      'due_date',
+    ],
+  })
     .then((dbDoozieData) => res.json(dbDoozieData))
     .catch((err) => {
       console.log(err);
@@ -11,23 +18,58 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  Doozie.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'description',
+      'due_date',
+    ]
+  })
+  .then(dbDoozieData => {
+    if (!dbDoozieData) {
+      res.status(404).json({ message: 'No to-do found with this id' });
+      return;
+    }
+    res.json(dbDoozieData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 // POST - create new doozie
 router.post('/', (req, res) => {
   Doozie.create({
-    userID: req.body.User_id,
+    User_id: req.body.User_id,
     title: req.body.title,
     description: req.body.description,
-    dueDate: req.body.due_date,
+    due_date: req.body.due_date
+  })
+  .then(dbDoozieData => res.json(dbDoozieData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   });
 });
 
 // PUT - update a doozie
 router.put('/:id', (req, res) => {
-  Doozie.update(req.body, {
-    where: {
-      id: req.params.id,
+  Doozie.update(
+    {
+      title: req.body.title
     },
-  })
+    {
+      where: {
+        id: req.params.id,
+      }
+    }
+  )
     .then((dbDoozieData) => {
       if (!dbDoozieData[0]) {
         res.status(404).json({ message: 'No to-do found with this id' });
@@ -43,6 +85,7 @@ router.put('/:id', (req, res) => {
 
 // DELETE doozie
 router.delete('/:id', (req, res) => {
+  console.log('id', req.params.id);
   Doozie.destroy({
     where: {
       id: req.params.id,
